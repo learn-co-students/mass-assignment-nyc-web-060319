9c7a43ce3cb7104bdc236f3da44cd77bbd5eae87
CindyChenx <chen_cx0128@hotmail.com>
2019-06-12 15:55:25 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -4,24 +4,12 @@ class Person
   :weight, :handed, :complexion, :t_shirt_size, 
   :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(attributes)
-    attributes.each{|key, value| self.send(("#{key}="), value)}
-
-    self.send(key=, value)
+  def initialize(attributes = nil)
+    if attributes
+      attributes.each do |key, value|
+        self.send("#{key}=", value)
+      end
+    end
   end
-
-
 end
 
-# bob_attributes = {name: "Bob", hair_color: "Brown"}
- 
-# bob = Person.new(bob_attributes)
-# bob.name       # => "Bob"
-# bob.hair_color # => "Brown"
- 
-# susan_attributes = {name: "Susan", height: "5'11\"", eye_color: "Green"}
- 
-# susan = Person.new(susan_attributes)
-# susan.name      # => "Susan"
-# susan.height    # => "5'11""
-# susan.eye_color # => "Green"
